c502793b33f075f92d4105e4de250abc8d0f5c04
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-05-09 18:24:27 +0200

    added @Value on Controller

--- src/main/java/develhope/esercizio90DeployEnvironmentsVariables02/YAMLConfig.java
+++ src/main/java/develhope/esercizio90DeployEnvironmentsVariables02/YAMLConfig.java
@@ -1,8 +1,10 @@
 package develhope.esercizio90DeployEnvironmentsVariables02;
 
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 @EnableConfigurationProperties
--- src/main/java/develhope/esercizio90DeployEnvironmentsVariables02/controllers/HelloController.java
+++ src/main/java/develhope/esercizio90DeployEnvironmentsVariables02/controllers/HelloController.java
@@ -2,17 +2,23 @@ package develhope.esercizio90DeployEnvironmentsVariables02.controllers;
 
 import develhope.esercizio90DeployEnvironmentsVariables02.YAMLConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @Autowired
-    private YAMLConfig yamlConfig;
+//    @Autowired
+//    private YAMLConfig yamlConfig;
+
+    @Value("${welcomeMsg}")
+    private String welcomeMsg;
+
 
     @GetMapping("/")
     public String welcome() {
-        return yamlConfig.getWelcomeMsg();
+        //return yamlConfig.getWelcomeMsg();
+        return welcomeMsg;
     }
 }
